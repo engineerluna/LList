3564ff4d30222cae5d2a0e69b3bc4144f530793d
engineerluna <engineer.luis.luna@gmail.com>
2014-02-02 20:52:16 -0800

    OutOutBoundException used in the test case

---
+++ ListIndexOutOfBoundsException.java
@@ -0,0 +1,12 @@
+/*	Luis Luna
+ *  Description: ListIndexOutOfBoudException that is used by the LinkedList 
+ *  class I created.  
+*/
+	
+public class ListIndexOutOfBoundsException extends RuntimeException
+	{
+		public ListIndexOutOfBoundsException(String s)
+			{
+				super(s);
+			}
+		}//End
